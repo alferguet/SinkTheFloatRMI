949983ab47f41b630b1f017b91b5f45f46cee47a
Ouronok <ouronok@gmail.com>
2017-11-14 20:12:59 +0100

    bugfixes

--- src/client/ImpCallbackCliente.java
+++ src/client/ImpCallbackCliente.java
@@ -14,7 +14,5 @@ public class ImpCallbackCliente extends UnicastRemoteObject implements IntCallba
 
     public void notify(String message) throws RemoteException{
         System.out.println(message);
-
-
     }
 }
